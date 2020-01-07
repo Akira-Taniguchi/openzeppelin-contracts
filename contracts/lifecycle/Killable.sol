393eeaeb1d8bb8a6a0ac16f1f446c067aa735873
Akira-Taniguchi <einstein19821108>
2020-01-07 09:58:46 +0900

    add killable

---
+++ contracts/lifecycle/Killable.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+
+contract Killable {
+	address payable public _owner;
+
+	constructor() internal {
+		_owner = msg.sender;
+	}
+
+	function kill() public {
+		require(msg.sender == _owner, "only owner method");
+		selfdestruct(_owner);
+	}
+}
+
